9825d94c11f925e95a61545101eb248bc36e7cda
Ben Tadiar <ben@bentadiar.co.uk>
2012-03-13 10:29:13 +0000

    Added debug views

---
+++ views/Notification.php
@@ -0,0 +1,5 @@
+<h3>Details of how the debug session was handled have been provided below:</h3>
+
+<?php foreach($responses as $object => $response): ?>
+	<strong><?php echo $object; ?></strong>: <?php echo $response; ?><br />
+<?php endforeach; ?>
---
+++ views/View.php
@@ -0,0 +1,1 @@
+An unrecoverable error occurred.
